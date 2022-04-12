1bbb2e411dcffc8aba8804328733028b8153b196
David Caldera <davidcaldera13@yahoo.com>
2022-04-11 21:59:59 -0500

    commit tree 04e58be1753be7718ba4900fab96f3a949870f4d

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,21 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        self.create(title: title)
+    end
 
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
 end
